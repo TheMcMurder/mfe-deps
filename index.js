be9fc1d2f70b2105534043232c0fadf7fd6a795c
Carlos Filoteo <c.filoteo12@gmail.com>
2020-11-30 09:37:48 -0700

    Initial implementation

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
-class MfeDepsWebpackPlugin {
-  constructor(options = {}) {}
-  apply(compiler) {}
-}
+/**
+ * The reason this is implemented as a webpack plugin is because:
+ * - statically analyzing the code can tell you about dependent modules BUT can't tell which are `external`
+ * - analyzing the final output can tell you which are `external` (depending on the format) but can't tell which are bundled dependencies
+ * By doing this in the bundler, you can get visibilty into both external and bundled dependencies.
+ */
+module.exports = class MfeDepsWebpackPlugin {
+  constructor() {
+    this.dependencies = [];
+    this.externals = [];
+  }
+  static name = 'MfeDepsWebpackPlugin';
+  apply(compiler) {
+    compiler.hooks.normalModuleFactory.tap(
+      MfeDepsWebpackPlugin.name,
+      (factory) => {
+        factory.hooks.parser
+          .for('javascript/auto')
+          .tap(MfeDepsWebpackPlugin.name, (parser) => {
+            parser.hooks.importSpecifier.tap(
+              MfeDepsWebpackPlugin.name,
+              (statement, source, exportName, identifierName) => {
+                this.dependencies.push({
+                  source,
+                  exportName,
+                });
+              }
+            );
+          });
+      }
+    );
 
-module.exports = MfeDepsWebpackPlugin;
+    compiler.hooks.done.tapAsync(
+      MfeDepsWebpackPlugin.name,
+      (stats, callback) => {
+        this.externals = Array.from(stats.compilation.modules)
+          .map((module) => (!module.externalType ? false : module.request))
+          .filter(Boolean);
+
+        const report = this.dependencies.map((dep) => ({
+          ...dep,
+          external: this.externals.includes(dep.source),
+        }));
+
+        console.log('report', report);
+        callback();
+      }
+    );
+  }
+};
