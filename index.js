8f07f86de7fe8412e3ec6d0eb0f69f84b43997d6
Carlos Filoteo <c.filoteo12@gmail.com>
2020-12-14 15:51:09 -0700

    Write report to file

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const { promises: fs } = require('fs');
+const { resolve } = require('path');
+
 /**
  * The reason this is implemented as a webpack plugin is because:
  * - statically analyzing the code can tell you about dependent modules BUT can't tell which are `external`
@@ -5,15 +8,20 @@
  * By doing this in the bundler, you can get visibilty into both external and bundled dependencies.
  */
 module.exports = class MfeDepsWebpackPlugin {
-  constructor(options) {
-    if (!options.name)
+  constructor({ name, meta }) {
+    if (!name)
       throw new Error(`${MfeDepsWebpackPlugin.name} requires a 'name' option`);
-
-    this.name = options.name;
+    this.name = name;
+    this.meta = meta;
     this.dependencies = [];
     this.externals = [];
   }
   static name = 'MfeDepsWebpackPlugin';
+
+  writeReport(reportPath, report) {
+    return fs.writeFile(reportPath, JSON.stringify(report, null, 2));
+  }
+
   apply(compiler) {
     compiler.hooks.normalModuleFactory.tap(
       MfeDepsWebpackPlugin.name,
@@ -27,8 +35,8 @@ module.exports = class MfeDepsWebpackPlugin {
                 const isLocalImport = source.startsWith('.');
                 if (!isLocalImport) {
                   this.dependencies.push({
-                    specifier: source,
-                    import: exportName,
+                    moduleName: source,
+                    specifier: exportName,
                     file: parser.state.module.rawRequest,
                   });
                 }
@@ -45,16 +53,40 @@ module.exports = class MfeDepsWebpackPlugin {
           .map((module) => (!module.externalType ? false : module.request))
           .filter(Boolean);
 
-        const dependencies = this.dependencies.map((dep) => ({
-          ...dep,
-          external: this.externals.includes(dep.source),
-        }));
+        const dependencies = this.dependencies
+          .map((dep) => ({
+            ...dep,
+            external: this.externals.includes(dep.moduleName),
+          }))
+          .reduce((acc, { specifier, moduleName, file, external }) => {
+            const existingImportIndex = acc.findIndex(
+              (d) => d.specifier === specifier && d.moduleName === moduleName
+            );
+            if (existingImportIndex !== -1) {
+              acc[existingImportIndex].files.push(file);
+            } else {
+              acc.push({
+                specifier,
+                moduleName,
+                files: [file],
+                external,
+              });
+            }
 
-        console.log({
+            return acc;
+          }, []);
+
+        const report = {
           name: this.name,
           dependencies,
-        });
-        callback();
+        };
+        if (this.meta) report.meta = this.meta;
+
+        this.writeReport(
+          // TODO: make this configurable
+          resolve(compiler.outputPath, 'mfe-deps.json'),
+          report
+        ).then(callback);
       }
     );
   }
