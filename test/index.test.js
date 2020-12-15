dc18e7de10389ab3d98ef4a86ea632d0a3d09285
Carlos Filoteo <c.filoteo12@gmail.com>
2020-12-15 08:28:49 -0700

    Additional tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,42 @@
 const webpack = require('webpack');
 const config = require('./fixtures/webpack.config.js');
+const { rmdirSync, existsSync } = require('fs');
+const { resolve } = require('path');
 
-it('Basic usage', (done) => {
+const FIXTURE = (f) => resolve(__dirname, 'fixtures', f);
+
+beforeAll(() => {
+  const fixtureDist = FIXTURE('dist');
+  try {
+    rmdirSync(fixtureDist, { recursive: true });
+    console.log(`Cleared ${fixtureDist} successfully.`);
+  } catch (err) {
+    console.error(`Error while deleting ${fixtureDist}.`);
+  }
+});
+
+test('Basic usage', (done) => {
   const compiler = webpack(config);
-
-  compiler.run((err, stats) => {
+  compiler.run((err) => {
+    expect(err).toBeFalsy();
+    expect(existsSync(FIXTURE('dist/mfe-deps.json'))).toBe(true);
     done();
   });
+});
+
+test('Failed config', () => {
+  expect(() => {
+    const compiler = webpack({
+      mode: 'development',
+      entry: FIXTURE('./index.js'),
+      output: {
+        path: FIXTURE('dist'),
+      },
+      plugins: [new MfeDepsWebpackPlugin({})],
+    });
+    compiler.run((err) => {
+      expect(err).toBeFalsy();
+      done();
+    });
+  }).toThrow();
 });
