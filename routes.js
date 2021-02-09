fa9d00e9c4ad06c506adf87dae73e578a94a7f31
Carlos Filoteo <c.filoteo12@gmail.com>
2021-02-08 21:10:49 -0700

    Initial commit

---
+++ routes.js
@@ -0,0 +1,25 @@
+async function routes(fastify, options) {
+  const collection = fastify.mongo.db.collection("reports");
+
+  fastify.get("/", async (request, reply) => {
+    const result = await collection.find().toArray();
+    if (result.length === 0) {
+      throw new Error("No documents found");
+    }
+    return result;
+  });
+
+  fastify.post(
+    "/report",
+    {
+      schema: {
+        body: { $ref: "mfe-deps-report-schema" },
+      },
+    },
+    async (request, reply) => {
+      return {};
+    }
+  );
+}
+
+module.exports = routes;
