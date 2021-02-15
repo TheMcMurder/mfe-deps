ee0c8626efe940a269d62061037fa52c2d4a60c8
Carlos Filoteo <c.filoteo12@gmail.com>
2021-02-15 16:31:44 -0700

    Return fake data as SSE

--- routes/root.js
+++ routes/root.js
@@ -1,14 +1,27 @@
 "use strict";
+const fs = require("fs").promises;
+const uniqWith = require("lodash.uniqwith");
+const isEqual = require("lodash.isequal");
+const dedupe = (arr) => uniqWith(arr, isEqual);
 
 module.exports = async function (fastify, opts) {
   const collection = fastify.mongo.db.collection("reports");
+  const indexHtml = await fs.readFile(__dirname + "/index.html");
 
   fastify.get("/", async (request, reply) => {
-    const result = await collection.find().toArray();
-    if (result.length === 0) {
-      throw new Error("No documents found");
-    }
-    return result;
+    reply.type("text/html").send(indexHtml);
+  });
+
+  fastify.get("/data", async (request, reply) => {
+    const chartData = visualize(MOCK_REPORT);
+    return reply.sse(
+      (async function* source() {
+        yield {
+          data: JSON.stringify(chartData),
+          event: "ping",
+        };
+      })()
+    );
   });
 
   fastify.post(
@@ -23,3 +36,99 @@ module.exports = async function (fastify, opts) {
     }
   );
 };
+
+const MOCK_REPORT = {
+  name: "@example/login",
+  dependencies: [
+    {
+      type: "ES6",
+      import: [
+        {
+          type: "default",
+          name: "React",
+        },
+      ],
+      module: "react",
+      source:
+        "/Users/cfiloteo/dev/mfe-dependency-analyzer/mfe-dependencies-test-repo/packages/login/src/example-login.tsx",
+      external: true,
+    },
+    {
+      type: "ES6",
+      import: [
+        {
+          type: "default",
+          name: "ReactDOM",
+        },
+      ],
+      module: "react-dom",
+      source:
+        "/Users/cfiloteo/dev/mfe-dependency-analyzer/mfe-dependencies-test-repo/packages/login/src/example-login.tsx",
+      external: true,
+    },
+    {
+      type: "ES6",
+      import: [
+        {
+          type: "default",
+          name: "singleSpaReact",
+        },
+      ],
+      module: "single-spa-react",
+      source:
+        "/Users/cfiloteo/dev/mfe-dependency-analyzer/mfe-dependencies-test-repo/packages/login/src/example-login.tsx",
+      external: false,
+    },
+    {
+      type: "ES6",
+      import: [
+        {
+          type: "default",
+          name: "React",
+        },
+      ],
+      module: "react",
+      source:
+        "/Users/cfiloteo/dev/mfe-dependency-analyzer/mfe-dependencies-test-repo/packages/login/src/root.component.tsx",
+      external: true,
+    },
+    {
+      type: "ES6",
+      import: [
+        {
+          type: "specifier",
+          name: "setPublicPath",
+        },
+      ],
+      module: "systemjs-webpack-interop",
+      source:
+        "/Users/cfiloteo/dev/mfe-dependency-analyzer/mfe-dependencies-test-repo/packages/login/src/set-public-path.tsx",
+      external: false,
+    },
+  ],
+};
+
+function visualize(report) {
+  const nodes = dedupe(
+    [
+      {
+        id: report.name,
+        group: "application",
+      },
+    ].concat(
+      report.dependencies.map((dep) => ({
+        id: dep.module,
+        group: dep.external ? "shared-dependency" : "dependency",
+      }))
+    )
+  );
+
+  const links = dedupe(
+    report.dependencies.map((dep) => ({
+      source: report.name,
+      target: dep.module,
+    }))
+  );
+
+  return { links, nodes };
+}
