03d199bca88dcf3c4b6f4fff120fd5ebab118bd3
Carlos Filoteo <c.filoteo12@gmail.com>
2021-02-20 18:59:34 -0700

    Strip out mongo

--- routes.js
+++ routes.js
@@ -1,40 +1,36 @@
 "use strict";
+
 const fs = require("fs").promises;
 const uniqWith = require("lodash.uniqwith");
 const isEqual = require("lodash.isequal");
 const dedupe = (arr) => uniqWith(arr, isEqual);
 const { EventIterator } = require("event-iterator");
-module.exports = async function (fastify, opts) {
-  const collection = fastify.mongo.db.collection("reports");
-  const changeStream = collection.watch();
 
+module.exports = async function (fastify, opts) {
   const indexHtml = await fs.readFile(__dirname + "/index.html");
 
   fastify.get("/", async (request, reply) => {
     reply.type("text/html").send(indexHtml);
   });
 
-  fastify.get("/data", async (request, reply) => {
-    const chartData = visualize(MOCK_REPORT);
+  fastify.get("/data", (request, reply) => {
     reply.sse(
       new EventIterator((push) => {
-        changeStream.on("change", (d) => push(d));
-        return () => changeStream.close();
+        // changeStream.on("change", (d) => push(d));
+        // return () => changeStream.close();
       })
     );
   });
 
-  fastify.post(
-    "/report",
-    {
-      schema: {
-        body: { $ref: "mfe-deps-report-schema#" },
-      },
-    },
-    async (request, reply) => {
-      return {};
+  fastify.post("/report", async (request, reply) => {
+    try {
+      const data = visualize(request.body.data);
+      // collection.insert(data);
+      reply.status(200);
+    } catch (e) {
+      return reply.status(500);
     }
-  );
+  });
 };
 
 const MOCK_REPORT = {
