4ac244b1eef28f274e848e147005c05819f2ee84
Carlos Filoteo <c.filoteo12@gmail.com>
2021-02-28 17:34:50 -0700

    Move routes after database

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const { FastifySSEPlugin } = require("fastify-sse-v2");
 
 module.exports = async function (fastify, opts) {
   fastify.register(FastifySSEPlugin);
-  fastify.register(require("./routes"));
   fastify.register(require("./database"));
+  fastify.register(require("./routes"));
 };
