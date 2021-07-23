081c58985e49b48b8247c0dd3afbc0eb03d61424
Carlos Filoteo <c.filoteo12@gmail.com>
2021-07-23 14:22:03 -0600

    Use env values throughout (WIP, likely to change)

--- packages/server/database.js
+++ packages/server/database.js
@@ -1,45 +1,47 @@
-"use strict";
+'use strict'
 
-const fp = require("fastify-plugin");
+const fp = require('fastify-plugin')
 
 // the use of fastify-plugin is required to be able
 // to export the decorators to the outer scope
 module.exports = fp(async function dbConnector(fastify, opts) {
-  fastify.register(require("fastify-mongodb"), {
-    url:
-      process.env.MONGODB_URL ||
-      "mongodb://mfe-deps-user:mfe-deps-password@localhost/dependencies",
-  });
+  const db_host =
+    process.env.NODE_ENV === 'production' ? 'mongodb' : 'localhost'
+  const DB_URL = `mongodb://${process.env.APP_DB_USER}:${process.env.APP_DB_PW}@${db_host}/${process.env.MONGO_INITDB_DATABASE}`
+
+  fastify.register(require('fastify-mongodb'), {
+    url: DB_URL,
+  })
 
   fastify.addSchema({
-    $id: "mfe-deps-report-schema",
-    type: "object",
-    required: ["name", "dependencies"],
+    $id: 'mfe-deps-report-schema',
+    type: 'object',
+    required: ['name', 'dependencies'],
     properties: {
-      name: { type: "string" },
+      name: { type: 'string' },
       dependencies: {
-        type: "array",
+        type: 'array',
         items: {
-          type: "object",
-          required: ["type", "import", "module", "source", "external"],
+          type: 'object',
+          required: ['type', 'import', 'module', 'source', 'external'],
           properties: {
-            type: { type: "string" },
+            type: { type: 'string' },
             import: {
-              type: "array",
+              type: 'array',
               items: {
-                type: "object",
+                type: 'object',
                 properties: {
-                  type: { type: "string" },
-                  name: { type: "string" },
+                  type: { type: 'string' },
+                  name: { type: 'string' },
                 },
               },
             },
-            module: { type: "string" },
-            source: { type: "string" },
-            external: { type: "boolean" },
+            module: { type: 'string' },
+            source: { type: 'string' },
+            external: { type: 'boolean' },
           },
         },
       },
     },
-  });
-});
+  })
+})
